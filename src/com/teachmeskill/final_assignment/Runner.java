05ce9964b89745725a9509f55ea60b576c4f3d83
PollyPetrova <polina.petrova.030325@gmail.com>
2024-01-05 20:29:59 +0300

    Third commit

--- src/com/teachmeskill/final_assignment/Runner.java
+++ src/com/teachmeskill/final_assignment/Runner.java
@@ -3,12 +3,23 @@ package com.teachmeskill.final_assignment;
 import com.teachmeskill.final_assignment.coder.Coder;
 import com.teachmeskill.final_assignment.service.authorization.AuthService;
 
+import java.util.Scanner;
+
 public class Runner {
 
     public static void main(String[] args) {
 
-        AuthService.doLogin("Polina", "Polina123987");
-/*        System.out.println(Coder.code("Polina"));
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter login:");
+        String login= scanner.nextLine();
+        System.out.println("Enter password:");
+        String password= scanner.nextLine();
+
+        //Right login:    Polina;
+        //      password: Polina123987;
+
+        AuthService.doLogin(login, password);
+        /*System.out.println(Coder.code("Polina"));
         System.out.println(Coder.code("Polina123987"));*/
 
     }
