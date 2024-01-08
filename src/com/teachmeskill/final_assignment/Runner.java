b0a89d43163af3111bb8603059d0e49ed07f2c2b
lissvet <elisse.rogow@gmail.com>
2024-01-08 11:45:17 +0300

    Comm-2 Lisa
    More services for working with files

--- src/com/teachmeskill/final_assignment/Runner.java
+++ src/com/teachmeskill/final_assignment/Runner.java
@@ -1,5 +1,4 @@
 package com.teachmeskill.final_assignment;
-
 import com.teachmeskill.final_assignment.service.AuthService;
 import com.teachmeskill.final_assignment.service.DocumentSortService;
 import com.teachmeskill.final_assignment.service.StatisticService;
@@ -13,28 +12,28 @@ public class Runner {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-//        System.out.println("Enter login:");
-//        String login = scanner.nextLine();
-//        System.out.println("Enter password:");
-//        String password = scanner.nextLine();
-//
-//        //Right login:    Polina;
-//        //      password: Polina123987;
-//
-//        AuthService.doLogin(login, password);
-//        /*System.out.println(Coder.code("Polina"));
-//        System.out.println(Coder.code("Polina123987"));*/
-//
-//        //Считываем путь к папке с файлами
-//        System.out.println("Enter path to the Directory:");
-//        String resourcePath = scanner.nextLine();
-//
-//        //вызов метода для сортировки файлов
-//        try {
-//            DocumentSortService.SortDocument(resourcePath);
-//        } catch (Exception e){
-//            //TODO запись в логгер с сообщением, датой и стектрейсом
-//      }
+        System.out.println("Enter login:");
+        String login = scanner.nextLine();
+        System.out.println("Enter password:");
+        String password = scanner.nextLine();
+
+        //Right login:    Polina;
+        //      password: Polina123987;
+
+        AuthService.doLogin(login, password);
+        /*System.out.println(Coder.code("Polina"));
+        System.out.println(Coder.code("Polina123987"));*/
+
+        //Считываем путь к папке с файлами
+        System.out.println("Enter path to the Directory:");
+        String resourcePath = scanner.nextLine();
+
+        //вызов метода для сортировки файлов
+        try {
+            DocumentSortService.SortDocument(resourcePath);
+        } catch (Exception e){
+            //TODO запись в логгер с сообщением, датой и стектрейсом
+      }
 
 
         //Вызов метода для суммирования транзакций из чеков
